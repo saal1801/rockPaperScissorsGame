106df5933ce003abf21084fa20c66e11721a78e6
Sarko <sarko.musik@live.se>
2021-01-24 14:58:53 +0100

    add class

--- src/rockPaperScissor/Main.java
+++ src/rockPaperScissor/Main.java
@@ -1,73 +1,11 @@
 package rockPaperScissor;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        String myMove;
-        int rand;
-        String opponentMove;
-        int userWin = 0;
-        int userLoss = 0;
-        int draw = 0;
-
-
-        Scanner scanner = new Scanner(System.in);
-
-        //Loop through and asking the user to enter a move
-        while (true){
-
-            System.out.println("Enter your move. Type in rock, paper or scissors. " +
-                    "If you want to exit the game, typ in quit ");
-
-            myMove = scanner.nextLine();
-
-            //Check if the user entered quit
-            if (myMove.equalsIgnoreCase("quit")){
-                System.out.println("Your Score " +"\n Win "+ userWin +"\nLoss "+ userLoss +"\nTied "+ draw);
-                break;
-
-            }
-
-            //Verify that myMove is valid
-            if ((!myMove.equalsIgnoreCase("rock")) &&
-                    (!myMove.equalsIgnoreCase("paper")) &&
-                    (!myMove.equalsIgnoreCase("scissors"))){
-                System.out.println("Your move isn't valid!");
-            }else {
-                //Randomly generate the opponents move (0, 1, 2)
-                rand = (int)(Math.random()*3);
-
-                if (rand == 0){
-                    opponentMove = "Rock";
-                }else if (rand == 1){
-                    opponentMove = "paper";
-                }else {
-                    opponentMove = "scissors";
-                }
-                System.out.println("Opponent move: " + opponentMove);
-
-                //Calculate who won or not
-                if (myMove.equalsIgnoreCase(opponentMove)){
-                    draw++;
-                    System.out.println("You tied " + draw);
-
-                }else if((myMove.equalsIgnoreCase("rock") && opponentMove.equalsIgnoreCase("scissors")) ||
-                        (myMove.equalsIgnoreCase("scissors") && opponentMove.equalsIgnoreCase("paper")) ||
-                        (myMove.equalsIgnoreCase("paper") && opponentMove.equalsIgnoreCase("rock"))) {
-                    userWin++;
-                    System.out.println("You won! " + userWin + " times");
-                }else {
-                    userLoss++;
-                    System.out.println("You lost! " + userLoss + " times");
-
-                }
-            }
-        }
-
-        System.out.println("Thanks for playing and see you later");
+        rockPaperScissors rockPaperScissor = new rockPaperScissors();
+        rockPaperScissor.rockPaperScissors();
     }
 }
 
---
+++ src/rockPaperScissor/rockPaperScissors.java
@@ -0,0 +1,73 @@
+package rockPaperScissor;
+
+import java.util.Scanner;
+
+public class rockPaperScissors {
+
+    private String myMove;
+    private int rand;
+    private String opponentMove;
+    private int userWin = 0;
+    private int userLoss = 0;
+    private int draw = 0;
+
+    public void rockPaperScissors(){
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Loop through and asking the user to enter a move
+        while (true){
+
+            System.out.println("Enter your move. Type in rock, paper or scissors. " +
+                    "If you want to exit the game, typ in quit ");
+
+            myMove = scanner.nextLine();
+
+            //Check if the user entered quit
+            if (myMove.equalsIgnoreCase("quit")){
+                System.out.println("Your Score " +"\n Win "+ userWin +"\nLoss "+ userLoss +"\nTied "+ draw);
+                break;
+
+            }
+
+            //Verify that myMove is valid
+            if ((!myMove.equalsIgnoreCase("rock")) &&
+                    (!myMove.equalsIgnoreCase("paper")) &&
+                    (!myMove.equalsIgnoreCase("scissors"))){
+                System.out.println("Your move isn't valid!");
+            }else {
+                //Randomly generate the opponents move (0, 1, 2)
+                rand = (int)(Math.random()*3);
+
+                if (rand == 0){
+                    opponentMove = "Rock";
+                }else if (rand == 1){
+                    opponentMove = "paper";
+                }else {
+                    opponentMove = "scissors";
+                }
+                System.out.println("Opponent move: " + opponentMove);
+
+                //Calculate who won or not
+                if (myMove.equalsIgnoreCase(opponentMove)){
+                    draw++;
+                    System.out.println("You tied " + draw);
+
+                }else if((myMove.equalsIgnoreCase("rock") && opponentMove.equalsIgnoreCase("scissors")) ||
+                        (myMove.equalsIgnoreCase("scissors") && opponentMove.equalsIgnoreCase("paper")) ||
+                        (myMove.equalsIgnoreCase("paper") && opponentMove.equalsIgnoreCase("rock"))) {
+                    userWin++;
+                    System.out.println("You won! " + userWin + " times");
+                }else {
+                    userLoss++;
+                    System.out.println("You lost! " + userLoss + " times");
+
+                }
+            }
+        }
+
+        System.out.println("Thanks for playing and see you later");
+    }
+}
